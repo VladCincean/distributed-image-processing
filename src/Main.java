52852791df745346e029c9895ee290ac6512bab5
Vlad CINCEAN <cvie1883@scs.ubbcluj.ro>
2018-01-09 23:07:53 +0200

    Added sepia(-ish)

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Image image = new Image("res/img1.jpg");
 
-        ImageProcessor.toGrayscaleAverage(image, "res/img1_gray.jpg");
+//        ImageProcessor.toGrayscaleAverage(image, "res/img1_gray.jpg");
+        ImageProcessor.toSepia(image, "res/img1_sepia.jpg");
     }
 }
